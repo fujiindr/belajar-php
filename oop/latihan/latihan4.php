86d40aadb02455391a056bc72d536a4fdf02a465
fujiindr <fujiindahrahayu@gmail.com>
2021-09-16 08:02:05 -0700

    upload

--- oop/latihan/latihan4.php
+++ oop/latihan/latihan4.php
@@ -1,38 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <FORM ACTION = "" METHOD = "POST" NAME = "input">
+            <tr>
+                <td>Jumlah Kaki Kucing </td>
+                <td> : <input type = "text" name = "kaki" required></td>
+            </tr>
+            <tr>
+                <td>Warna Kucing </td>
+                <td> : <input type = "text" name = "warna" required></td>
+            </tr>
+            <tr>
+                <td>
+                    <input type = "submit" name = "input" value = "Input">
+                </td>
+            </tr></form>
+        </table><br>
+</body>
+</html>
+
 <?php
+if (isset($_POST['input'])) {
+    $kaki = $_POST['kaki'];
+    $warna = $_POST['warna'];
 
-class kucing
-{
-    public $warna;
-    public $kucing;
-    protected $kucing1 = "3 Kaki";
-    protected $kucing2 = "4 Kaki";
-    protected $kucing3 = "5 Kaki";
-
-    public function kucing()
+    class kucing
     {
-        $kucing1 = $this->kucing1;
-        $kucing2 = $this->kucing2;
-        if ($kucing1 == "3 Kaki") {
-            $a = "Kucing Cingked";
-        } elseif ($kucing2 == "4 Kaki") {
-            $b = "Kucing Normal";
-        } else {
-            $c = "Siluman Kucing";
+        public function __construct($kaki)
+        {
+            $this->kaki = $kaki;
+
         }
-        return $a;
+
+        public function kakiKucing()
+        {
+            if ($this->kaki > 4) {
+                $a = "Siluman";
+            } elseif ($this->kaki < 4) {
+                $a = "Cingked";
+            } else {
+                $a = "Normal";
+            }
+            return $a;
+        }
     }
+    $kucing = new kucing($kaki);
+    ?>
+    <table>
+    <tr>
+        <td><?php echo "Jumlah Kaki : " . $kucing->kaki . "<br>"; ?> </td>
+    </tr>
+    <tr>
+        <td><?php echo "Info : Kucing " . $warna . " " . $kucing->kakiKucing() . "<br>"; ?> </td>
+    </tr>
+</table>
+<?php
 }
-/*class kakiKucing extends kucing
-{
-public function kaki()
-{
-return $this->kucing;
-}
-}*/
-$kucing = new kucing();
-//$login = new b();
-$kucing->warna = "Merah";
-echo "Kucing : " . $kucing->warna . "<br>";
-//echo "Kaki Kucing : " . $kucing->kucing1 . "<br>";
-//echo "Email : " . $login->email() . "<br>";
-echo $kucing->kucing() . "<br>";
+?>
+
